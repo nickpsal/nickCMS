e4a21569258837e5857343a87c1b8ea2216aff33
nickpsal <nickpsal@gmail.com>
2020-08-26 14:04:49 +0300

    commit

--- admin/includes/view_all_users.php
+++ admin/includes/view_all_users.php
@@ -33,43 +33,43 @@
         </tr>
     </thead>
     <tbody>
-    <?php
-        $query = "SELECT * FROM users ORDER BY user_id DESC";
-        $select_all_users = mysqli_query($conn, $query);
-        if ($select_all_users) {
-            while ($row = mysqli_fetch_assoc($select_all_users)) {
-                $user_id = $row['user_id'];
-                $username = $row['user_username'];
-                $firstname = $row['user_firstname'];
-                $lastname = $row['user_lastname'];
-                $email = $row['user_email'];
-                $date = date("d/m/Y", strtotime($row['user_register']));
-                $image = $row['user_image'];
-                $role = $row['user_role'];
-                $status = $row['user_status'];
-                echo "<tr>";
-                echo "<td>{$user_id}</td>";
-                echo "<td><a href='users.php?action=edit_user&user_id={$user_id}'</a>$username</td>";
-                echo "<td>{$firstname}</td>";  
-                echo "<td>{$lastname}</td>";
-                echo "<td>{$email}</td>";  
-                $path = "../images/" . $username;  
-                echo "<td><img class='img-responsive' src='$path/$image' alt='image'></td>";
-                echo "<td>{$role}</td>";
-                echo "<td>{$date}</td>";
-                if ($role != 'Admin') {
-                    if ($status == "Unapproved") {
-                        echo "<td>Disable</td>";
-                        echo "<td><a href='users.php?approved={$user_id}'</a>Enable</td>";
-                    }else if ($status == "Approved") {
-                        echo "<td>Enable</td>";
-                        echo "<td><a href='users.php?unapproved={$user_id}'</a>Disable</td>";
+        <?php
+            $query = "SELECT * FROM users ORDER BY user_id DESC";
+            $select_all_users = mysqli_query($conn, $query);
+            if ($select_all_users) {
+                while ($row = mysqli_fetch_assoc($select_all_users)) {
+                    $user_id = $row['user_id'];
+                    $username = $row['user_username'];
+                    $firstname = $row['user_firstname'];
+                    $lastname = $row['user_lastname'];
+                    $email = $row['user_email'];
+                    $date = date("d/m/Y", strtotime($row['user_register']));
+                    $image = $row['user_image'];
+                    $role = $row['user_role'];
+                    $status = $row['user_status'];
+                    echo "<tr>";
+                    echo "<td>{$user_id}</td>";
+                    echo "<td><a href='users.php?action=edit_user&user_id={$user_id}'</a>$username</td>";
+                    echo "<td>{$firstname}</td>";  
+                    echo "<td>{$lastname}</td>";
+                    echo "<td>{$email}</td>";  
+                    $path = "../images/" . $username;  
+                    echo "<td><img class='img-responsive' src='$path/$image' alt='image'></td>";
+                    echo "<td>{$role}</td>";
+                    echo "<td>{$date}</td>";
+                    if ($role != 'Admin') {
+                        if ($status == "Unapproved") {
+                            echo "<td>Disable</td>";
+                            echo "<td><a href='users.php?approved={$user_id}'</a>Enable</td>";
+                        }else if ($status == "Approved") {
+                            echo "<td>Enable</td>";
+                            echo "<td><a href='users.php?unapproved={$user_id}'</a>Disable</td>";
+                        }
+                        echo "<td><a href='users.php?delete={$user_id}'</a>Delete</td>";
                     }
-                    echo "<td><a href='users.php?delete={$user_id}'</a>Delete</td>";
+                    echo "</tr>";                              
                 }
-                echo "</tr>";                              
             }
-        }
-    ?>
+        ?>
     </tbody>                          
 </table>
